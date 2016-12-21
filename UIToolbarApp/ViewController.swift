9d1daed226229ef4e10f38242f4fb71264b629a1
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2016-12-22 00:03:11 +0530

    Initial Commit

---
+++ UIToolbarApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIToolbarApp
+//
+//  Created by Shrawan Shinde on 22/12/16.
+//  Copyright © 2016 Shrawan Shinde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
